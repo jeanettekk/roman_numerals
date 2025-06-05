cc2e0ae217ee18fb4c7ff9e015eb977a5a767499
jeanettekk <khojeanette@gmail.com>
2025-06-05 14:49:42 +0100

    initial commit

---
+++ roman_numerals.rb
@@ -0,0 +1,17 @@
+def numeral_method(numeral, numeral_number)
+  if numeral.start_with?('I')
+    numeral_number - 1
+  else
+    numeral.count('I') + numeral_number
+  end
+end
+
+def roman_to_numeral(numeral)
+  if numeral.include?('V')
+    numeral_method(numeral, 5)
+  elsif numeral.include?('X')
+    numeral_method(numeral, 10)
+  else
+    numeral.length
+  end
+end
---
+++ tests.rb
@@ -0,0 +1,31 @@
+require_relative 'roman_numerals'
+
+def assert_that(actual, expected)
+  return unless actual != expected
+
+  raise "Assertion failed: expected #{expected.inspect}, but got #{actual.inspect}"
+end
+
+# Parameterized test
+numbers = [
+  ['I', 1],
+  ['II', 2],
+  ['III', 3],
+  ['IV', 4],
+  ['V', 5],
+  ['VI', 6],
+  ['VII', 7],
+  ['VIII', 8],
+  ['IX', 9],
+  ['X', 10],
+  ['XII', 12]
+]
+
+# Parameterized test
+numbers.each do |roman, num|
+  assert_that(roman_to_numeral(roman), num)
+  puts "✅ #{roman} = #{num}"
+rescue StandardError => e
+  puts "❌ #{roman} != #{num} : #{e.message}"
+end
+
