4849400b57032782094d99d229561bf1833666c3
jeanettekk <khojeanette@gmail.com>
2025-08-07 11:38:43 +0100

    changed the name of the roman_special_chars

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,12 +1,12 @@
-def numeral_method(numeral, numeral_number, roman_to_numeral_map)
+def numeral_method(numeral, numeral_number, roman_special_chars)
   if numeral.start_with?('I')
     numeral_number - 1
   elsif numeral.include?('IV')
-    numeral_number + roman_to_numeral_map[:V] - 1
+    numeral_number + roman_special_chars[:V] - 1
   elsif numeral.include?('XV')
     numeral_number + 5 + numeral.scan('I').length
   elsif numeral.include?('IX')
-    numeral_number + roman_to_numeral_map[:X] - 1
+    numeral_number + roman_special_chars[:X] - 1
   else
     numeral_count = numeral.scan('I').length
     numeral_count + numeral_number
@@ -14,14 +14,14 @@ def numeral_method(numeral, numeral_number, roman_to_numeral_map)
 end
 
 def roman_to_numeral(numeral)
-  roman_to_numeral_map = {
+  roman_special_chars = {
     V: 5,
     X: 10
   }
   if numeral.include?('X')
-    numeral_method(numeral, 10, roman_to_numeral_map)
+    numeral_method(numeral, 10, roman_special_chars)
   elsif numeral.include?('V')
-    numeral_method(numeral, 5, roman_to_numeral_map)
+    numeral_method(numeral, 5, roman_special_chars)
   else
     numeral.length
   end
