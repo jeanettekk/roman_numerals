926c15a30447c5b21a1666a83a8690d51fb02e57
jeanettekk <khojeanette@gmail.com>
2025-10-23 11:33:18 +0100

    Passed AAAA test with discount

--- kata_exercises/checkout/check_out.rb
+++ kata_exercises/checkout/check_out.rb
@@ -9,7 +9,9 @@ class CheckOut
   def scan(item)
     a_count = item.count('A')
 
-    if a_count == 3
+    if a_count > 3
+      set_total(180)
+    elsif a_count == 3
       set_total(130)
     else
       set_total(a_count * 50)
--- kata_exercises/checkout/checkout_tests.rb
+++ kata_exercises/checkout/checkout_tests.rb
@@ -5,7 +5,7 @@ require_relative 'check_out'
 RSpec.describe CheckOut do
   let(:check_out) { CheckOut.new }
 
-  items = { '' => 0, 'A' => 50, 'AA' => 100, 'AAA' => 130 }.freeze
+  items = { '' => 0, 'A' => 50, 'AA' => 100, 'AAA' => 130, 'AAAA' => 180 }.freeze
 
   items.each do |item, expected_sum|
     it "scanning item '#{item}' should result in total of #{expected_sum}" do
