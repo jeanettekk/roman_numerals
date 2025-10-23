08497d24c54eb5c904a4e1af7729e8d3eb6230dd
jeanettekk <khojeanette@gmail.com>
2025-10-23 11:10:45 +0100

    removing old tests

--- kata_exercises/checkout/checkout_tests.rb
+++ kata_exercises/checkout/checkout_tests.rb
@@ -7,34 +7,11 @@ RSpec.describe CheckOut do
 
   let(:items) { { '' => 0, 'A' => 50, 'AA' => 100, 'AAA' => 150 } }
 
-  it 'empty scanner should have total amount of 0' do
-    check_out
-    expect(check_out.total).to equal(0)
-  end
-
   it 'scanning empty items should not change the total' do
     items.each do |item, expected_sum|
       check_out
       check_out.scan(item)
       expect(check_out.total).to equal(expected_sum)
     end
-  end
-
-  it 'scans A to equal 50' do
-    check_out
-    check_out.scan('A')
-    expect(check_out.total).to equal(50)
-  end
-
-  it 'scans AA to a total of 100' do
-    check_out
-    check_out.scan('AA')
-    expect(check_out.total).to equal(100)
-  end
-
-  it 'scans AAA to a total of 150' do
-    check_out
-    check_out.scan('AAA')
-    expect(check_out.total).to equal(150)
   end
 end
