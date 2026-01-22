00e9ef063bee986e0a6554a4c08146f09eb71a68
jeanettekk <khojeanette@gmail.com>
2026-01-22 10:56:15 +0000

    all passing so starting fresh

--- kata_exercises/checkout/check_out.rb
+++ kata_exercises/checkout/check_out.rb
@@ -1,28 +1,24 @@
 # frozen_string_literal: true
 class CheckOut
-  attr_reader :total # only a getter for external use
+  class << self
+    attr_accessor :total
 
-  def initialize
-    @total = 0 # internal state
-  end
+    def scan(item)
+      # Config
+      a_price = 50
+      a_discount_amount = 20
+      a_discount_quantity = 3
+      # Calculation
+      a_count = item.count('A')
+      number_of_discounts = (a_count / a_discount_quantity).floor
+      discount = number_of_discounts * a_discount_amount
+      total_without_discount = a_count * a_price
 
-  def scan(item)
-    # Config
-    a_price = 50
-    a_discount_amount = 20
-    a_discount_quantity = 3
-    # Calculation
-    a_count = item.count('A')
-    number_of_discounts = (a_count / a_discount_quantity).floor
-    discount = number_of_discounts * a_discount_amount
-    total_without_discount = a_count * a_price
+      @total = total_without_discount - discount
+    end
 
-    set_total(total_without_discount - discount)
-  end
-
-  private
-
-  def set_total(value)
-    @total = value # internal "setter", not visible outside
+    def reset
+      @total = 0
+    end
   end
 end
--- kata_exercises/checkout/checkout_tests.rb
+++ kata_exercises/checkout/checkout_tests.rb
@@ -3,14 +3,14 @@
 require_relative 'check_out'
 
 RSpec.describe CheckOut do
-  let(:check_out) { CheckOut.new }
 
+  # items = { 'AAAAA' => 230 }.freeze
   items = { '' => 0, 'A' => 50, 'AA' => 100, 'AAA' => 130, 'AAAA' => 180, 'AAAAA' => 230 }.freeze
 
   items.each do |item, expected_sum|
     it "scanning item '#{item}' should result in total of #{expected_sum}" do
-      check_out.scan(item)
-      expect(check_out.total).to equal(expected_sum)
+      CheckOut.scan(item)
+      expect(CheckOut.total).to equal(expected_sum)
     end
   end
 end
