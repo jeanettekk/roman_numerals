8d38f6d868e43697dc4614a762c9452ae316aa7d
jeanettekk <khojeanette@gmail.com>
2026-01-22 12:02:44 +0000

    ABC test refactor

--- kata_exercises/checkout/checkout_tests.rb
+++ kata_exercises/checkout/checkout_tests.rb
@@ -6,7 +6,8 @@ RSpec.describe CheckOut do
 
   # items = { 'AAABB' => 175 }.freeze
   items = { '' => 0, 'A' => 50, 'AA' => 100, 'AAA' => 130, 'AAAA' => 180, 'AAAAA' => 230, 'AAAAAA' => 260,
-            'AB' => 80, 'AAABB' => 175, 'AAAAAABBBBB' => 380, 'AAAAAAABBBBB' => 430, 'ABC' => 100 }.freeze
+            'AB' => 80, 'AAABB' => 175, 'AAAAAABBBBB' => 380, 'AAAAAAABBBBB' => 430,
+            'ABC' => 100 }.freeze
 
   items.each do |item, expected_sum|
     it "scanning item '#{item}' should result in total of #{expected_sum}" do
