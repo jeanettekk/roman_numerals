dd1f4d3b830115d6c696d05f79bf3ab3b710c310
jeanettekk <khojeanette@gmail.com>
2026-01-22 11:44:25 +0000

    C is passing

--- kata_exercises/checkout/check_out.rb
+++ kata_exercises/checkout/check_out.rb
@@ -13,18 +13,26 @@ class CheckOut
       b_discount_amount = 15
       b_discount_quantity = 2
 
+      c_price = 20
+
       # Calculation
       a_count = item.count('A')
       b_count = item.count('B')
+      c_count = item.count('C')
 
       number_of_discounts_a = (a_count / a_discount_quantity).floor
       number_of_discounts_b = (b_count / b_discount_quantity).floor
       discount_a = number_of_discounts_a * a_discount_amount
       discount_b = number_of_discounts_b * b_discount_amount
+      discount_c = 0
       total_without_discount_a = a_count * a_price
       total_without_discount_b = b_count * b_price
+      total_without_discount_c = c_count * c_price
 
-      @total = (total_without_discount_a + total_without_discount_b) - (discount_a + discount_b)
+      total_amount_all = total_without_discount_a + total_without_discount_b + total_without_discount_c
+      total_discount_all = discount_a + discount_b + discount_c
+
+      @total = total_amount_all - total_discount_all
     end
 
     def reset
