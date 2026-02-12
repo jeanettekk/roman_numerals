cf1174611ee47257453afa62804ac75fd4905910
jeanettekk <khojeanette@gmail.com>
2026-02-12 11:31:13 +0000

    Refactor new method for C

--- kata_exercises/checkout/check_out.rb
+++ kata_exercises/checkout/check_out.rb
@@ -16,17 +16,13 @@ class CheckOut
       c_price = 20
 
       # Calculation
-      b_count = item.count('B')
-      c_count = item.count('C')
+      discount_a, total_without_discount_a = calculate_discounted_prices(a_discount_amount, a_discount_quantity, a_price, item, 'A')
+      discount_b, total_without_discount_b = calculate_discounted_prices(b_discount_amount, b_discount_quantity, b_price, item, 'B')
+      total_without_discount_c = calculate_prices_without_discount(c_price, item)
 
-      discount_a, total_without_discount_a = calculate_prices(a_discount_amount, a_discount_quantity, a_price, item, 'A')
-      discount_b, total_without_discount_b = calculate_prices(b_discount_amount, b_discount_quantity, b_price, item, 'B')
-
-      discount_c = 0
-      total_without_discount_c = c_count * c_price
 
       total_amount_all = total_without_discount_a + total_without_discount_b + total_without_discount_c
-      total_discount_all = discount_a + discount_b + discount_c
+      total_discount_all = discount_a + discount_b
 
       @total = total_amount_all - total_discount_all
     end
@@ -37,7 +33,12 @@ class CheckOut
 
     private
 
-    def calculate_prices(discount_amount, discount_quantity, price_per_item, item, letter)
+    def calculate_prices_without_discount(c_price, item)
+      c_count = item.count('C')
+      c_count * c_price
+    end
+
+    def calculate_discounted_prices(discount_amount, discount_quantity, price_per_item, item, letter)
       letter_count = item.count(letter)
       number_of_discounts = (letter_count / discount_quantity).floor
       discount = number_of_discounts * discount_amount
